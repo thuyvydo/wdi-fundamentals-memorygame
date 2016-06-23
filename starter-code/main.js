0ec58cc07f7f5622bb6d4098c2b0e85a5421841f
thuyvydo <tweevy.do@gmail.com>
2016-06-23 09:13:11 -0700

    updated js for exercise ch 9

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,8 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = cardOne;
 var cardFour = cardTwo;
+
+if (cardOne === cardThree) {
+	alert("You found a match!");
+}
+else alert("Sorry try again");
